aa70ca9659c5fb7647970562c680b7c1d025000c
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-07 13:03:24 +0000

    Now prints out even values

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
- *
- * @author 
+import java.util.Scanner;
+ /**
+ * Asks the user for a start and end value then prints out all even values between those values.
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // ask the user to enter a start counting value
+    System.out.println("What number do you want to begin counting from?");
+    // create a variable for user input
+    int startValue = input.nextInt();
+    
+    // ask the user to enter a value to stop at
+    System.out.println("What number do you want to end counting at?");
+    // create a variable for user input
+    int endValue = input.nextInt();
+    
+    // repeats code until reaches endValue and adds 2 each time
+    for(int count = startValue; count <= endValue; count+= 2){
+      // if starting value is odd adds one
+      if(count%2 == 1){
+        count++;
+      }
+      // prints out values
+      System.out.println(count);
+    }
+      
     
   }
 }
+ 
